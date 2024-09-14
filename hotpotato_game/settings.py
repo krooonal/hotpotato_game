e2abf08d0a31339da83d4b649273f8fbcb49b564
Krunal Patel <krunalkachadia@gmail.com>
2024-09-13 20:48:35 -0700

    Allow all hosts

--- hotpotato_game/settings.py
+++ hotpotato_game/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*q2e^erm&0j9pifskf1b(auut39&6!jy9^$!v2bw3%^pd!*$g3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
