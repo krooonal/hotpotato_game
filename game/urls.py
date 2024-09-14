55c6b5947397a5afd849cdf9ace4ffab2bf2e084
Krunal Patel <krunalkachadia@gmail.com>
2024-09-13 21:44:30 -0700

    Removing names from paths.

--- game/urls.py
+++ game/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('start/', views.start_game, name='start_game'),
-    path('pass/', views.pass_potato, name='pass_potato'),
+    path('start/', views.start_game),
+    path('pass/', views.pass_potato),
 ]
