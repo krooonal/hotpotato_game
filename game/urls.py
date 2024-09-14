bfbad0e894935dd7b42da592e55501c34da693ac
Krunal Patel <krunalkachadia@gmail.com>
2024-09-13 22:32:50 -0700

    Comment out everything

--- game/urls.py
+++ game/urls.py
@@ -1,9 +1,10 @@
 # game/urls.py
 from django.urls import path
-from game.views import start_game, pass_potato, index
+from game.views import index
+# from game.views import start_game, pass_potato, index
 
 urlpatterns = [
     path('', index),
-    path('start/', start_game),
-    path('pass/', pass_potato),
+    # path('start/', start_game),
+    # path('pass/', pass_potato),
 ]
--- game/views.py
+++ game/views.py
@@ -1,69 +1,69 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # game/views.py
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import GameData
-from django.core.mail import send_mail
-import json
+# from django.views.decorators.csrf import csrf_exempt
+# from .models import GameData
+# from django.core.mail import send_mail
+# import json
 
-game_data = GameData()
+# game_data = GameData()
 
 def index(request):
     return JsonResponse({"game_id": "y"})
 
 # @csrf_exempt
-def start_game(request):
-    if request.method == 'POST':
-        body = json.loads(request.body)
-        user_email = body.get('user_email')
-        new_game = game_data.create_game(user_email)
-        game_id = new_game["game_id"]
+# def start_game(request):
+#     if request.method == 'POST':
+#         body = json.loads(request.body)
+#         user_email = body.get('user_email')
+#         new_game = game_data.create_game(user_email)
+#         game_id = new_game["game_id"]
 
-        send_mail(
-            subject='You have created a Hot Potato!',
-            message=f'You have 24 hours to pass the hot potato.\n' +
-            f'Pass the potato using ' +
-            f'http://localhost:8080/passpotato.html?' +
-            f'user={user_email}&gameId={game_id}',
-            html_message=f'You have 24 hours to pass the hot potato.\n' +
-            f'Pass the potato using ' +
-            f'<a href=http://localhost:8080/passpotato.html?' +
-            f'user={user_email}&gameId={game_id}>this link</a>',
-            from_email='noreply@krooonal.com',
-            recipient_list=[user_email]
-        )
-        return JsonResponse({"game_id": new_game["game_id"], "message": "Game started!"})
-    else:
-        return JsonResponse({"game_id": "x"})
+#         send_mail(
+#             subject='You have created a Hot Potato!',
+#             message=f'You have 24 hours to pass the hot potato.\n' +
+#             f'Pass the potato using ' +
+#             f'http://localhost:8080/passpotato.html?' +
+#             f'user={user_email}&gameId={game_id}',
+#             html_message=f'You have 24 hours to pass the hot potato.\n' +
+#             f'Pass the potato using ' +
+#             f'<a href=http://localhost:8080/passpotato.html?' +
+#             f'user={user_email}&gameId={game_id}>this link</a>',
+#             from_email='noreply@krooonal.com',
+#             recipient_list=[user_email]
+#         )
+#         return JsonResponse({"game_id": new_game["game_id"], "message": "Game started!"})
+#     else:
+#         return JsonResponse({"game_id": "x"})
 
 # @csrf_exempt
-def pass_potato(request):
-    if request.method == 'POST':
-        body = json.loads(request.body)
-        game_id = body.get('game_id')
-        current_user = body.get('current_user')
-        new_user = body.get('new_user')
-        result = game_data.pass_potato(game_id, current_user, new_user)
+# def pass_potato(request):
+#     if request.method == 'POST':
+#         body = json.loads(request.body)
+#         game_id = body.get('game_id')
+#         current_user = body.get('current_user')
+#         new_user = body.get('new_user')
+#         result = game_data.pass_potato(game_id, current_user, new_user)
         
-        if "error" in result:
-            return JsonResponse({"error": result["error"]})
+#         if "error" in result:
+#             return JsonResponse({"error": result["error"]})
         
-        # Send email notification to the new user
-        send_mail(
-            subject='You have the Hot Potato!',
-            message=f'{current_user} passed you a hot potato.\n' +
-            f'You have 24 hours to pass the hot potato.\n' +
-            f'Pass the potato using ' +
-            f'http://localhost:8080/passpotato.html?' +
-            f'user={new_user}&gameId={game_id}',
-            html_message=f'{current_user} passed you a hot potato.\n' +
-            f'You have 24 hours to pass the hot potato.\n' +
-            f'Pass the potato using ' +
-            f'<a href=http://localhost:8080/passpotato.html?' +
-            f'user={new_user}&gameId={game_id}>this link</a>',
-            from_email='noreply@krooonal.com',
-            recipient_list=[new_user]
-        )
-        return JsonResponse({"message": f"Potato passed to {new_user}!"})
+#         # Send email notification to the new user
+#         send_mail(
+#             subject='You have the Hot Potato!',
+#             message=f'{current_user} passed you a hot potato.\n' +
+#             f'You have 24 hours to pass the hot potato.\n' +
+#             f'Pass the potato using ' +
+#             f'http://localhost:8080/passpotato.html?' +
+#             f'user={new_user}&gameId={game_id}',
+#             html_message=f'{current_user} passed you a hot potato.\n' +
+#             f'You have 24 hours to pass the hot potato.\n' +
+#             f'Pass the potato using ' +
+#             f'<a href=http://localhost:8080/passpotato.html?' +
+#             f'user={new_user}&gameId={game_id}>this link</a>',
+#             from_email='noreply@krooonal.com',
+#             recipient_list=[new_user]
+#         )
+#         return JsonResponse({"message": f"Potato passed to {new_user}!"})
 
